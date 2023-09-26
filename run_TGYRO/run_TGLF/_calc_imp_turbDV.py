616f7b178be312d8307cff22d8c8bdd6658d5dd8
cjperks7 <cjperk@mit.edu>
2023-09-26 17:54:53 -0400

    Added tools to compare growth rate spectra from a number of TGLF runs

--- run_TGYRO/run_TGLF/_calc_imp_turbDV.py
+++ run_TGYRO/run_TGLF/_calc_imp_turbDV.py
@@ -8,9 +8,13 @@ from TGYRO flux-matched profiles
 # Modules
 from portals.gacode_tools 	import TGLFmodule
 import numpy as np
+from transport_world.plot_utils import plt_slider
+from scipy.interpolate  import interp1d
+import os
 
 __all__ = [
     'calc_imp_turbDV',
+    'cmp_tglf_spectra',
     ]
 
 ############################################
@@ -32,7 +36,19 @@ def calc_imp_turbDV(
     restart = True,     # True = from scratch, False = already have output
     TGLFsettings = 4,   # 1 -> SAT1 old, 2 -> SAT0, 3 -> SAT1geo, 4 -> SAT2, 5 -> SAT2em
     # PLotting
-    plt_all = None,
+    plt_all = False,
+    plt_spec = False,
+    plt_type = 'freq',
+    plt_lmts = {
+        0:{
+            'xlim':None,
+            'ylim':None,
+            },
+        1:{
+            'xlim':None,
+            'ylim':None,
+            },
+        }
     ):
 
     # Initialize TGLF class at the locations
@@ -54,15 +70,196 @@ def calc_imp_turbDV(
         trace = [cs,amu],
         )
 
-    # Plots the TGLF analysis results
-    if plt_all:
-        tglf.plotAnalysis(
-            labels=['analysis1'],
-            analysisType='Z',
+    # Plots growth rate spectra
+    if plt_spec:
+        _plot(
+            tglfs=[tglf],
+            plt_type=plt_type,
+            plt_lmts=plt_lmts
             )
+        # Plots the TGLF analysis results
+        if plt_all:
+            tglf.plotAnalysis(
+                labels=['analysis1'],
+                analysisType='Z',
+                )
 
     return {
         'rhot': tglf.rhos, # sq. norm. tor. flux, dim(tglf_rhot,)
         'DZ': tglf.scans['analysis1']['DZ'], # [m^2/s], dim(tglf_rhot,)
-        'VZ': tglf.scans['analysis1']['VZ'] # [m/s], dim(tglf_rhot,)
+        'VZ': tglf.scans['analysis1']['VZ'], # [m/s], dim(tglf_rhot,)
+        'tglf': tglf
         }
+
+############################################
+#
+#               Compare TGLF spectra
+#
+############################################
+
+def cmp_tglf_spectra(
+    in_path=None,
+    fgacodes=None,
+    name_sims=None,
+    folder=None,
+    # Plotting
+    plt_type = 'freq',
+    plt_lmts = {
+        0:{
+            'xlim':None,
+            'ylim':None,
+            },
+        1:{
+            'xlim':None,
+            'ylim':None,
+            },
+        }
+    ):
+
+    # Initializes
+    tglfs = []
+
+    # Loop over simulations
+    for ii, name_sim in enumerate(name_sims):
+        dout = calc_imp_turbDV(
+            fgacode = os.path.join(
+                in_path,
+                fgacodes[ii]
+                ),
+            folder = os.path.join(
+                in_path,
+                'output_TGLF',
+                folder,
+                name_sim
+                ),
+            subfolder = name_sim,
+            restart=False,
+            )
+
+        tglfs.append(dout['tglf'])
+
+    # Plotting
+    _plot(
+        tglfs=tglfs,
+        plt_type=plt_type,
+        plt_lmts=plt_lmts
+        )
+
+
+
+############################################
+#
+#               Plotting
+#
+############################################
+
+def _plot(
+    tglfs=None,
+    plt_type = 'freq',
+    plt_lmts = None,
+    ):
+
+    # Initializes data to plot
+    freq_mstr = np.zeros((len(tglfs), 31, 28)) # dim(ntglf, nrho, nky)
+    gamma_mstr = np.zeros((len(tglfs), 31, 28)) # dim(ntglf, nrho, nky)
+
+    # Unique labels
+    dzlabels = []
+    ky_mstr = []
+
+    # Loop over TGLF results
+    for tglf in tglfs:
+        # Key to results
+        keyr = list(tglf.results.keys())[1]
+
+        dzlabels.append(
+            keyr.split('_')[1]
+            + '_'
+            + keyr.split('_')[3].split('R')[0]
+            )
+
+        # Obtains wavenumber mesh
+        ky = tglf.results[keyr]['ky'] # dim(nrho,nky)
+
+        # Interpolates onto uniform ky mesh
+        ky_mstr.append(np.mean(ky, axis=0)) # dim(nky,)
+
+    ky_mstr = np.mean(ky_mstr, axis=0) # dim(nky,)
+
+    # Loop over TGLF results
+    for jj, tglf in enumerate(tglfs):
+
+        # Key to reuslts
+        keyr = list(tglf.results.keys())[1]
+
+        # Obtains the radial mesh
+        rhot = tglf.results[keyr]['x'] # dim(nrho,)
+
+        # Obtains wavenumber mesh
+        ky = tglf.results[keyr]['ky'] # dim(nrho,nky)
+
+        # Obtains frequency and growth rate spectra
+        freq = tglf.results[keyr]['freq'] # dim(nrho,nky)
+        gamma = tglf.results[keyr]['gamma'] # dim(nrho, nky)
+
+        # Loop over nrho
+        for ii in np.arange(freq.shape[0]):
+
+            freq_mstr[jj,ii,:] = interp1d(
+                ky[ii,:],
+                freq[ii,:],
+                bounds_error = False,
+                fill_value = (freq[ii,0], freq[ii,-1])
+                )(ky_mstr) # dim(nrho, nky)
+
+            gamma_mstr[jj,ii,:] = interp1d(
+                ky[ii,:],
+                gamma[ii,:],
+                bounds_error = False,
+                fill_value = (gamma[ii,0], gamma[ii,-1])
+            )   (ky_mstr) # dim(nrho, nky)
+
+    if plt_type == 'freq':
+        data1 = freq_mstr
+        data2 = gamma_mstr
+        lab1 = r"$\omega$ [$C_s/a$]"
+        lab2 = r"$\gamma$ [$C_s/a$]"
+    elif plt_type == 'mixing':
+        data1 = freq_mstr/ky_mstr[None,None,:]
+        data2 = gamma_mstr/ky_mstr[None,None,:]
+        lab1 = r"$\omega/(k_\theta\rho_s)$ [$C_s/a$]"
+        lab2 = r"$\gamma/(k_\theta/\rho_s)$ [$C_s/a$]"
+
+    # Plots slider
+    plt_slider(
+        xxx=ky_mstr,
+        yyy=rhot,
+        dzzz=data1,
+        xxlabel=r"$k_\theta\rho_s$",
+        yylabel=r"$\rho_t$",
+        zzlabel= lab1,
+        dzlabels=dzlabels,
+        plt_sum=False,
+        y_line = 0.0,
+        xscale='log',
+        yscale='symlog',
+        xlim=plt_lmts['omega']['xlim'],
+        ylim=plt_lmts['omega']['ylim'],
+        )
+
+    plt_slider(
+        xxx=ky_mstr,
+        yyy=rhot,
+        dzzz=data2,
+        xxlabel=r"$k_\theta\rho_s$",
+        yylabel=r"$\rho_t$",
+        zzlabel=lab2,
+        dzlabels=dzlabels,
+        plt_sum=False,
+        xscale='log',
+        yscale='log',
+        xlim=plt_lmts['gamma']['xlim'],
+        ylim=plt_lmts['gamma']['ylim'],
+        )
+
+
