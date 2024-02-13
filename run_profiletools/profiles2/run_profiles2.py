3be35846742afc430ca2094360ebfecb8bb41be7
cjperks7 <cjperk@mit.edu>
2024-02-13 12:38:22 -0500

    learned how to use profiletools to fit CMOD ne/Te data using GPR

---
+++ run_profiletools/profiles2/run_profiles2.py
@@ -0,0 +1,12 @@
+'''
+
+Scripts to run profiletools to fit CMOD data using python2.7
+
+
+'''
+
+# Module
+import sys
+path2profs = '/home/sciortino/usr/pythonmodules'
+sys.path.append(path2profs)
+import profiletools, eqtools, gptools, TRIPPy
